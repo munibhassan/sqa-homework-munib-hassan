1731c65894c22de66547e240317a70a0203aa2c7
Munib Hassan <munib.nexus@gmail.com>
2026-07-23 03:14:25 +0500

    resolve githib actions issue for deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 60000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
